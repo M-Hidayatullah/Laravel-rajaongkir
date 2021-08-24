0513128fe93d11f55eba70e92178e96ff1dfcf42
M-Hidayatullah <hidayatullahblk@gmail.com>
2021-08-24 18:36:50 +0800

    integrasi rajaongkir

---
+++ app/Http/Controllers/RajaOngkirController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class RajaOngkirController extends Controller
+{
+    /**
+     * API_KEY
+     * 
+     * @var string
+     */
+    protected $API_KEY = 'efdba4e62ead6601dabb1f489e9a8ee4';
+
+    public function getProvinces()
+    {
+        
+        $response = Http::withHeaders([
+            'key' => $this->API_KEY
+        ])->get('https://api.rajaongkir.com/starter/province');
+
+        $provinces = $response['rajaongkir']['results'];
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Get All Provinces',
+            'data'    => $provinces    
+        ]);
+    }
+    
+    /**
+     * getCities
+     *
+     * @param  mixed $id
+     * @return void
+     */
+    public function getCities($id)
+    {
+        
+        $response = Http::withHeaders([
+            'key' => $this->API_KEY
+        ])->get('https://api.rajaongkir.com/starter/city?&province='.$id.'');
+
+        $cities = $response['rajaongkir']['results'];
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Get City By ID Provinces : '.$id,
+            'data'    => $cities    
+        ]);
+    }
+        
+    /**
+     * checkOngkir
+     *
+     * @param  mixed $request
+     * @return void
+     */
+    public function checkOngkir(Request $request)
+    {
+        $response = Http::withHeaders([
+            'key' => $this->API_KEY
+        ])->post('https://api.rajaongkir.com/starter/cost', [
+            'origin'            => $request->origin,
+            'destination'       => $request->destination,
+            'weight'            => $request->weight,
+            'courier'           => $request->courier
+        ]);
+
+        $ongkir = $response['rajaongkir']['results'];
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Result Cost Ongkir',
+            'data'    => $ongkir    
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/provinces', [App\Http\Controllers\RajaOngkirController::class, 'getProvinces']);
+Route::get('/cities/{id}', [App\Http\Controllers\RajaOngkirController::class, 'getCities']);
+Route::post('/checkOngkir', [App\Http\Controllers\RajaOngkirController::class, 'checkOngkir']);
+
